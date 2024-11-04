985f5b166f459d26662e0130ad810e7781b570b2
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-11-04 10:29:01 -0300

    Step 25.

--- pyramid-generator/pyramid.js
+++ pyramid-generator/pyramid.js
@@ -3,7 +3,3 @@ let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
 rows[2] = 10;
-let cities = ["London", "New York", "Mumbai"];
-console.log(cities);
-cities[cities.length-1] = "Mexico City";
-console.log(cities);
