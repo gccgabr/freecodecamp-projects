43b3ddb0738b48658beb84e8a0d66dcbcc89d368
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-11-04 10:07:14 -0300

    Removed the new initializations the the console logs.

--- pyramid-generator/pyramid.js
+++ pyramid-generator/pyramid.js
@@ -1,8 +1,1 @@
 let character = 'Hello';
-console.log(character);
-character = "World";
-console.log(character);
-let secondCharacter;
-secondCharacter = "Test";
-console.log(secondCharacter);
-secondCharacter = character;
