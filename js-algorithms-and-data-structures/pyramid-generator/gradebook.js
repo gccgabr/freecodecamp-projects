b2f9d0e691db5fd0c5ce7568f7f39492882c5ea3
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-11-06 11:59:59 -0300

    Step 4.

--- js-algorithms-and-data-structures/pyramid-generator/gradebook.js
+++ js-algorithms-and-data-structures/pyramid-generator/gradebook.js
@@ -42,3 +42,20 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
+
+function studentMsg(totalScores, studentScore) {
+  if (hasPassingGrade(studentScore)) {
+    return "Class average: "
+      + getAverage(totalScores)
+      + ". Your grade: "
+      + getGrade(studentScore)
+      + ". You passed the course.";
+  } else {
+    return "Class average: "
+      + getAverage(totalScores)
+      + ". Your grade: "
+      + getGrade(studentScore)
+      + ". You failed the course.";
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
