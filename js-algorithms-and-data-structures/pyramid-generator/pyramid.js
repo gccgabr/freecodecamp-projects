1bfffe9259cac1986ed808c08e28133d7c20f309
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-11-06 11:41:36 -0300

    Step 117.

--- js-algorithms-and-data-structures/pyramid-generator/pyramid.js
+++ js-algorithms-and-data-structures/pyramid-generator/pyramid.js
@@ -11,26 +11,3 @@ for (let i = 1; i <= count; i++) {
 		rows.push(padRow(i, count));
 	}
 }
-
-/*
-while (rows.length < count) {
-	rows.push(padRow(rows.length+1, count));
-}
-*/
-
-/*
-for (let i = count; i > 0; i--) {
-	rows.push(padRow(i, count));
-}
-*/
-
-function padRow(rowNumber, rowCount) {
-	return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
-}
-
-let result = "";
-console.log(result);
-
-for (const row of rows) {
-	result = result + "\n" + row;
-}
