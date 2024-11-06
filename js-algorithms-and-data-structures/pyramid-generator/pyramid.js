8c77011bac4ee9e6531ce4e6d964f2df449d9d2a
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-11-06 11:12:43 -0300

    Step 77.

--- js-algorithms-and-data-structures/pyramid-generator/pyramid.js
+++ js-algorithms-and-data-structures/pyramid-generator/pyramid.js
@@ -2,10 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+/*
 for (let i = 1; i <= count; i++) {
 	rows.push(padRow(i, count));
 	// Comment.
 }
+*/
 
 function padRow(rowNumber, rowCount) {
 	return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
