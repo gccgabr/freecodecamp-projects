fc3a3830cf88ccbc591d757977fd0c3da4eba0af
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-12-02 11:23:43 -0300

    Step 149.

--- js-algorithms-and-data-structures/role-playing-game/script.js
+++ js-algorithms-and-data-structures/role-playing-game/script.js
@@ -256,5 +256,5 @@ function winGame() {
 function getMonsterAttackValue(level) {
 	const hit (level * 5) - (Math.floor(Math.random() * xp));
 	console.log(hit);
-	return hit;
+	return hit ? hit : 0;
 }
