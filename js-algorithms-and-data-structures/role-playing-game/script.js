95351f5dc73e9066b1ccac2d9a6636895605a2bd
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-12-02 11:26:33 -0300

    Step 151.

--- js-algorithms-and-data-structures/role-playing-game/script.js
+++ js-algorithms-and-data-structures/role-playing-game/script.js
@@ -208,6 +208,8 @@ function attack() {
 	health -= getMonsterAttackValue(monsters[fighting].level);
 	if (isMonsterHit()) {
 		monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+	} else {
+		text.innerText += " You miss.";
 	}
 	healthText.innerText = health;
 	monsterHealthText.innerText = monsterHealth;
