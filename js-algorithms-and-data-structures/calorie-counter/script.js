6349edbb0a6cff4ff1f683e40a27a0b19885e627
Gabriel Costa Chaves <gcc.gabr@gmail.com>
2024-12-10 12:04:12 -0300

    Step 76.

--- js-algorithms-and-data-structures/calorie-counter/script.js
+++ js-algorithms-and-data-structures/calorie-counter/script.js
@@ -58,4 +58,5 @@ function calculateCalories(e) {
 	const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
 	const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
 	const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+	const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
